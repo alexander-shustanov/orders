a759b58cc1f7efaa927c0258e27cc440c627f57e
alexander.shustanov <alexander.shustanov@gmail.com>
2025-04-18 16:21:08 +0400

    yc deploy pipeline

--- ops/yc-deploy/main.tf
+++ ops/yc-deploy/main.tf
@@ -4,9 +4,27 @@ terraform {
             source = "yandex-cloud/yandex"
         }
     }
+
+    backend "s3" {
+        endpoints = {
+            s3 = "https://storage.yandexcloud.net"
+        }
+        bucket = "amplicode-tests-tf-state"
+        region = "ru-central1"
+        key    = "orders/orders.tfstate"
+
+        skip_region_validation      = true
+        skip_credentials_validation = true
+        skip_requesting_account_id  = true
+        skip_s3_checksum            = true
+
+    }
 }
 
-provider "yandex" { folder_id = var.folder_id }
+provider "yandex" {
+    folder_id = var.folder_id
+    service_account_key_file = var.service_account_key_file
+}
 
 data "yandex_vpc_network" "default" {
     name = "default"
--- ops/yc-deploy/variables.tf
+++ ops/yc-deploy/variables.tf
@@ -1,9 +1,12 @@
 variable "folder_id" {
-    type = string
+    type    = string
     default = "b1g4nr518ju8ej3sfm91"
 }
 
-
 variable "orders_version" {
+    type = string
+}
+
+variable "service_account_key_file" {
     type = string
 }
